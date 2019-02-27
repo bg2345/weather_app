9725e51c1466f80d5117fa628d6573ecc0a6e3ba
bg2345 <bgardynski@gmail.com>
2019-02-27 15:07:26 -0500

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+export default class Config {
+  constructor() {
+    this.API_KEY = 'd0772f717082733b6a963f9b79fa4a86';
+
+  }
+
+  getKey() {
+    return this.API_KEY;
+  }
+}
---
+++ static/js/app.js
@@ -0,0 +1,50 @@
+// all imports at top, import config to access api key
+import Config from '../../config.js';
+
+// create an instance of the Config class
+let config = new Config();
+
+
+// render the navbar into the menuheader
+$.get('../../components/header.html', function(res) {
+  $('header').html(res);
+});
+
+
+function convert(num) {
+  let deg = (num - 273.15) * (9/5) + 32;
+  return deg.toFixed(2);
+}
+
+
+// create a function that logs the city entered in the form
+function searchCity() {
+  // log the city in in "city_search"
+  let city = $('#city_search').val();
+
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${config.getKey()}`;
+
+  $.get(url, function(res) {
+    console.log(res);
+
+
+    $('#city_name').text(`${res.name}, ${res.sys.country}`);
+    $('#high').html(`${convert(res.main.temp_max)}&deg;`);
+    $('#low').html(`${convert(res.main.temp_min)}&deg;`);
+    $('#forecast').text(`${res.weather[0].description}`);
+    $('#humidity').text(`${res.main.humidity}%`);
+
+    $('#weather-info').css('display', 'block');
+  });
+}
+
+// check to see if the submit button is pressed, if it is, stop the event from refreshing the page, and call search city
+$('#btn-primary').click(function(e) {
+  // prevent default stops form from refreshing page
+  e.preventDefault();
+  searchCity();
+});
+
+
+// make search information not appear until they submit a city_name
+$('#weather-info').css('display', 'none');
